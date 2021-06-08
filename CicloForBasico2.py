703c7f4b6c19b0bc81ebc0b0fa79aee67d5a33c9
Julio Fuentes <jefuente@uc.cl>
2021-06-08 18:32:12 -0400

    Actualizacion

--- CicloForBasico2.py
+++ CicloForBasico2.py
@@ -1,7 +1,7 @@
 #1. Tamaño grande: dada una lista, escriba una función que cambie todos los números positivos de la lista a "big".
 #Ejemplo: biggie_size ([- 1, 3, 5, -5]) devuelve la misma lista, pero cuyos valores son ahora [-1, "big", "big", -5]
 
-def biggie_size(a):
+def biggie_size(a=[]):
     for i in range(0, len(a)):
         if a[i]>0:
             a[i]="big"
@@ -14,7 +14,7 @@ print(biggie_size(lista))
 #Ejemplo: count_positives ([- 1,1,1,1]) cambia la lista original a [-1,1,1,3] y la devuelve
 #Ejemplo: count_positives ([1,6, -4, -2, -7, -2]) cambia la lista a [1,6, -4, -2, -7,2] y la devuelve
 
-def count_positives(a):
+def count_positives(a=[]):
     contador=0
     for i in range(0, len(a)):
         if a[i]>0:
@@ -28,7 +28,7 @@ print(count_positives(lista))
 #Ejemplo: sum_total ([1,2,3,4]) debería devolver 10
 #Ejemplo: sum_total ([6,3, -2]) debería devolver 7
 
-def sum_total(a):
+def sum_total(a=[]):
     suma=0
     for i in range(0, len(a)):
         suma+=a[i]   
@@ -39,7 +39,7 @@ print(sum_total(lista))
 #4. Promedio : crea una función que toma una lista y devuelve el promedio de todos los valores.
 #Ejemplo: el promedio ([1,2,3,4]) debería devolver 2.5
 
-def promedio(a):
+def promedio(a=[]):
     suma=0
     for i in range(0, len(a)):
         suma+=a[i]   
@@ -52,7 +52,7 @@ print(promedio(lista))
 #Ejemplo: la longitud ([37,2,1, -9]) debería devolver 4
 #Ejemplo: longitud ([]) debería devolver 0
 
-def longitud(a):   
+def longitud(a=[]):   
     return len(a)
 lista =[1,2,3,4]
 print(longitud(lista))
@@ -62,7 +62,7 @@ print(longitud(lista))
 #Ejemplo: mínimo ([37,2,1, -9]) debería devolver -9
 #Ejemplo: mínimo ([]) debería devolver False
 
-def minimo(a):
+def minimo(a=[]):
     if a ==[]:
         return False
     min=a[0]
@@ -80,7 +80,7 @@ print(minimo(vacia))
 #Ejemplo: máximo ([37,2,1, -9]) debería devolver 37
 #Ejemplo: máximo ([]) debería devolver False
 
-def maximo(a):
+def maximo(a=[]):
     if a ==[]:
         return False
     max=a[0]
@@ -96,7 +96,9 @@ print(maximo(vacia))
 #8. Análisis final : crea una función que tome una lista y devuelva un diccionario que tenga la suma total, promedio, mínimo, máximo y longitud de la lista.
 #Ejemplo: ultimate_analysis ([37,2,1, -9]) debería devolver {'total': 31, 'promedio': 7.75, 'minimo': -9, 'maximo': 37, 'longitud': 4}
 
-def ultimate_analysis(a):
+def ultimate_analysis(a=[]):
+    if a ==[]:
+        return False
     suma=0
     min=max=a[0]
     for i in range(0, len(a)):
@@ -116,7 +118,7 @@ print(ultimate_analysis(lista))
 #9. Lista inversa : crea una función que tome una lista y la devuelva con los valores invertidos. Haz esto sin crear una segunda lista. (Se sabe que este desafío aparece durante las entrevistas técnicas básicas).
 #Ejemplo: reverse_list ([37,2,1, -9]) debería devolver [-9,1,2,37]
 
-def reverse_list(a):
+def reverse_list(a=[]):
     mitad=int(len(a)/2)
     for i in range(0, mitad): #hasta el largo de la mitad
         b=a[len(a)-1-i] #comenzamos asignando a b el valor del ultimo elemento y vamos retrocediendo
